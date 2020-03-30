fc3099e37d3909a362ac7e99cc045a0929ccb264
Rohit Kumar <rkumar@dimagi.com>
2020-03-30 12:54:50 +0530

    Adding Alerting rules and targets

--- test/integration/alertmanager/serverspec/test_spec.rb
+++ test/integration/alertmanager/serverspec/test_spec.rb
@@ -1,6 +1,12 @@
 require 'serverspec'
 set :backend, :exec
 
+config_check_command = '/usr/local/bin/amtool  check-config /etc/alertmanager/alertmanager.yml'
+
+describe command (config_check_command) do
+  its (:exit_status) {should eq 0}
+end
+
 describe service('alertmanager') do
   it { should be_enabled }
   it { should be_running }
