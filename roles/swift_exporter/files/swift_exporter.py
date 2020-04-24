f7230bdd2c7e9ed9e2b38f3d0fc56376efc14f6c
Rohit Kumar <rkumar@dimagi.com>
2020-04-24 13:46:07 +0530

    simplify code

--- roles/swift_exporter/files/swift_exporter.py
+++ roles/swift_exporter/files/swift_exporter.py
@@ -4,15 +4,15 @@ import yaml
 import logging
 import sys
 from prometheus_client import start_http_server, Gauge
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 import time
 
 #Define prometheus_metrics
-gauge_container_count = Gauge('swift_container_count','Number of container in an account', ['account_tags'])
-gauge_object_count = Gauge('swift_object_count', 'Number of object in an account', ['account_tags'])
-gauge_used_bytes = Gauge('swift_used_bytes', 'Used bytes in an account', ['account_tags'])
-gauge_quota_bytes = Gauge('swift_quota_bytes', 'quota bytes in an account', ['account_tags'])
-gauge_container_connect = Gauge('swift_container_connect', 'Connection to container', ['account_tags'])
+gauge_container_count = Gauge('swift_container_count','Number of container in an account', ['account'])
+gauge_object_count = Gauge('swift_object_count', 'Number of object in an account', ['account'])
+gauge_used_bytes = Gauge('swift_used_bytes', 'Used bytes in an account', ['account'])
+gauge_quota_bytes = Gauge('swift_quota_bytes', 'quota bytes in an account', ['account'])
+gauge_container_connect = Gauge('swift_container_connect', 'Connection to container', ['account'])
 
 ## Read yaml config
 def get_config(configfile):
@@ -43,24 +43,24 @@ def swift_connect(user,key,authurl):
     return resp_headers,containers
 
 ## Get container size        
-def calculate_container_size(resp_headers,containers):
-        container_count = resp_headers['x-account-container-count']
-        object_count = resp_headers['x-account-object-count']
-        used_bytes = resp_headers['x-account-bytes-used']
-        quota_bytes = resp_headers['x-account-meta-quota-bytes']
-        return container_count, object_count, used_bytes, quota_bytes    
+def get_account_stats(resp_headers,containers):
+    container_count = resp_headers['x-account-container-count']
+    object_count = resp_headers['x-account-object-count']
+    used_bytes = resp_headers['x-account-bytes-used']
+    quota_bytes = resp_headers['x-account-meta-quota-bytes']
+    container_obj =  namedtuple('_',['container_count', 'object_count', 'used_bytes', 'quota_bytes'])(container_count, object_count, used_bytes, quota_bytes)
+    return container_obj
 
 ## Prometheus_metrics conversion
-def prometheus_metrics(container_obj):
-    for tag, account in container_obj:
-        if account['can_connect'] == 1:
-            gauge_container_count.labels(account_tags=tag).set(account['container_count'])
-            gauge_object_count.labels(account_tags=tag).set(account['object_count'])
-            gauge_used_bytes.labels(account_tags=tag).set(account['used_bytes'])
-            gauge_quota_bytes.labels(account_tags=tag).set(account['quota_bytes'])
-            gauge_container_connect.labels(account_tags=tag).set(account['can_connect'])
-        else:
-            gauge_container_connect.labels(account_tags=tag).set(account['can_connect'])
+def prometheus_metrics(container_obj,tag,can_connect):
+    if can_connect == 1:
+        gauge_container_count.labels(account=tag).set(10)
+        gauge_object_count.labels(account=tag).set(container_obj.object_count)
+        gauge_used_bytes.labels(account=tag).set(container_obj.used_bytes)
+        gauge_quota_bytes.labels(account=tag).set(container_obj.quota_bytes)
+        gauge_container_connect.labels(account=tag).set(can_connect)
+    else:
+        gauge_container_connect.labels(account=tag).set(can_connect)
 
 # Main Funtion
 if __name__ == "__main__": 
@@ -70,20 +70,10 @@ if __name__ == "__main__":
     logging.info("Starting swift exporter on port : %s", config_obj['listen_port']) 
 
     while True:
-        container_dict = defaultdict(list)
         for obj in config_obj['swift_accounts']:
-            jsonobj = defaultdict(list)
             resp_headers, containers = swift_connect(obj['user'], obj['key'], obj['authurl'])
             if resp_headers and containers:
-                container_count, object_count, used_bytes, quota_bytes = calculate_container_size(resp_headers, containers)
-                jsonobj['object_count'] = object_count
-                jsonobj['container_count'] = container_count
-                jsonobj['used_bytes'] =  used_bytes
-                jsonobj['quota_bytes'] = quota_bytes
-                jsonobj['can_connect'] = 1
-                container_dict[obj['tag']] =jsonobj
-            else: 
-                jsonobj['can_connect'] = 0
-                container_dict[obj['tag']] =jsonobj
-        prometheus_metrics(container_dict)
-        time.sleep(20)
+                can_connect = 1
+                container_obj = get_account_stats(resp_headers, containers)
+                prometheus_metrics(container_obj,obj['tag'],can_connect)
+        time.sleep(300)
